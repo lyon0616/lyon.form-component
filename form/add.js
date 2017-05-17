860daa7d4069301baa3a7fb22719e93a6daf5799
lyon0616 <15100198045@163.com>
2017-05-17 11:33:56 +0800

    Delete add.js

--- form/add.js
+++
@@ -1,5 +0,0 @@
-$(function(){
-    // $("configOptions").append("<div id='app"+tempid+"'><diy-component> </diy-component></div>")
-    // console.log($("body"));
-    // $("body").append("<div><label for="">label</label><input type="text" name="" value=""></div><div><label for="">id</label><input type="text" name="" value=""></div><div><label for="">class</label><input type="text" name="" value=""></div><div id="inputType"><span>type</span><div><label for="">text</label><input type="radio" name="" value=""></div><div><label for="">textarea</label><input type="radio" name="" value=""></div><div><label for="">date</label><input type="radio" name="" value=""></div><div><label for="">number</label><input type="radio" name="" value=""></div><div><label for="">select</label><input type="radio" name="" value=""></div><div><label for="">file</label><input type="radio" name="" value=""></div><div><label for="">button</label><input type="radio" name="" value=""></div></div><div style="display:none">我是隐藏的</div><div><div id="btn">更多</div><div id="addBtn">添加列表项</div></div>")
-});
